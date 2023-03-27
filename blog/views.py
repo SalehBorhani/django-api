84d479f6e8bb271b94b6b4cc780d025862219493
SalehBorhani <borhanisaleh6@gmail.com>
2023-03-27 12:32:10 +0330

    Updated.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def blog_view(request):
     return render(request, 'blog/blog-home.html')
 
 def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+    context = {'title': 'Saleh has won the gold medal of the WorldSkills 2026 in CloudComputing!'}
+    return render(request, 'blog/blog-single.html', context=context)
